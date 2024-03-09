2b4ac2142453e6bf24605e7f2535e439eefbc2f2
A R DANISH <helloardanish@gmail.com>
2024-03-09 16:17:49 +0530

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,31 @@
+import os
+import re
+import pyperclip
+
+with open('jsonobject.txt', 'r') as file:
+    string_data = file.read()
+
+
+def format_json(json_string):
+    pattern = r'([\{\,]\s*)(\w+?)\s*:'
+    
+    def add_quotes(match):
+        return f'{match.group(1)}"{match.group(2)}":'
+    
+    formatted_json = re.sub(pattern, add_quotes, json_string)
+    
+    formatted_json = formatted_json.replace("'", '"')
+    
+    return formatted_json
+
+formatted_json = format_json(string_data)
+
+pyperclip.copy(formatted_json) # optional to copy on clipboard
+
+#if os.path.isfile('formattedjson.txt'):
+ #   print("formatted.txt file already exists. Replacing its contents.")
+
+with open('formattedjson.txt', 'w') as output_file:
+    output_file.write(formatted_json)
+
+print(formatted_json)
